65da1a43c396f2ff5d50ca0857f02d3c8d79de70
Gereon <gereon.risse@gmail.com>
2024-02-29 19:00:52 +0100

    Installed flowbite-svelte and refactored navbar to flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,33 @@
 <script>
     import "../app.css";
     import logo from '$lib/assets/logo.png';
-    import Fa from 'svelte-fa'
-    import { faBars } from '@fortawesome/free-solid-svg-icons'
+    import {Fa} from 'svelte-fa'
     import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
+    import {Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte";
 </script>
 
-<nav class="py-2 px-4 sm:px-16 flex flex-row bg-purple-950 justify-between drop-shadow-lg">
-    <!-- Logo + Name -->
-    <div class="flex flex-row gap-2 items-center">
+<Navbar class="bg-purple-950 drop-shadow-lg">
+    <NavBrand href="/">
         <img src="{logo}" alt="logo" class="h-12" />
-        <h1 class="text-xl text-purple-100">Grober Unfug e.V.</h1>
-    </div>
+        <h1 class="ml-4 text-xl text-purple-100">Grober Unfug e.V.</h1>
+    </NavBrand>
+    <NavHamburger class="text-purple-300 hover:text-purple-200 hover:bg-purple-800" />
+    <NavUl classUl="bg-purple-950">
+        <NavLi class="text-purple-300 hover:text-purple-200 hover:text-purple-200 hover:bg-purple-700" href="/">Home</NavLi>
+        <NavLi class="text-purple-300 hover:text-purple-200 hover:text-purple-200 hover:bg-purple-700" href="/downloads">Downloads</NavLi>
 
-    <!-- Links and Socials -->
-    <div class="flex flex-row items-center">
-        <!-- Mobile -->
-        <div class="block sm:hidden">
-            <!-- Hamburger -->
-            <button class="block">
-                <Fa icon={faBars} size="lg" color="#ffffff" />
-            </button>
-        </div>
+        <span class="border-l-2 border-purple-900"></span>
 
-        <!-- Desktop -->
-        <div class="hidden sm:flex flex-row text-purple-300">
-            <!-- Links -->
-            <div class="flex flex-row">
-                <a href="/" class="px-4 hover:text-purple-200">Home</a>
-                <a href="/downloads" class="px-4 hover:text-purple-200">Downloads</a>
-            </div>
-
-            <!-- Socials -->
-            <div class="flex flex-row ml-6 pl-6 border-l-2 border-purple-900">
-                <a href="https://www.instagram.com/grober_unfug_ev" target="_blank" class="px-4">
-                    <Fa icon={faInstagram} size="lg" color="#c084fc" scale="1.15" />
-                </a>
-                <a href="https://www.facebook.com/PsyParkBonn" target="_blank" class="px-4">
-                    <Fa icon={faFacebook} size="lg" color="#c084fc" />
-                </a>
-            </div>
-        </div>
-    </div>
-
-</nav>
+        <NavLi class="text-purple-300 hover:text-purple-200 hover:bg-purple-900 flex flex-row gap-8" href="/downloads">
+            <a href="https://www.instagram.com/grober_unfug_ev" target="_blank">
+                <svg class="h-6 w-6 fill-purple-400 hover:fill-purple-300" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><g transform="translate(224 256)" transform-origin="112 0"><g transform="translate(0,0) scale(1.15,1.15)"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" transform="translate(-224 -256)"></path></g></g></svg>
+            </a>
+            <a href="https://www.facebook.com/PsyParkBonn" target="_blank">
+                <svg class="h-6 w-6 fill-purple-400 hover:fill-purple-300" viewBox="0 0 512 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(256 256)" transform-origin="128 0"><g transform="translate(0,0) scale(1,1)"><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" transform="translate(-256 -256)"></path></g></g></svg>
+            </a>
+        </NavLi>
+    </NavUl>
+</Navbar>
 
 <main class="h-screen bg-purple-900 text-purple-100 p-4">
     <slot />
